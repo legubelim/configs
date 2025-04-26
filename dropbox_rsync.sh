90c3f31d0fb8c54bd895de7a252f8e97a4a0482b
legubelim <glegrand@gmx.net>
2025-04-26 18:53:50 +0200

    rsync to Dropbox

---
+++ dropbox_rsync.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Crontab entry
+# crontab -e
+# 0 * * * * /home/glg/scripts/dropbox_rsync.sh >> /home/glg/scripts/dropbox_rsync.log 2>&1
+set -e
+
+cd /home/glg/ownCloud
+
+INCLUDES=`find . -name '_dropbox'  | sed "s/\.\/\(.*\)\_dropbox$/\1 /" | tr -d "\n"`
+echo "rsync -avR $INCLUDES /home/glg/Dropbox"
+
+rsync -avR $INCLUDES /home/glg/Dropbox
+
